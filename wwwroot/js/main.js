37fc051e5c02a46f0887b1915962e468e20b3f29
rebeckalscarlsson <rebecka.ls.carlsson@gmail.com>
2019-01-30 14:43:55 +0100

    Fixat klart och snyggat till 'visa alla nyheter'

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -12,86 +12,25 @@ async function clickSeed() {
     }
 }
 
-//UPPGIFT
-//visa nyhet:
-//skapa funktion till showallnews
-//visa alla nyheter i consollen samt printas på sidan
-
 async function clickShowAllNews() {
-    alert('Går in i metoden..');
 
     let response = await fetch(`api/news/`); //automatiskt get-call
 
-    //vill lägga till datan i div id="newsList"
-
     if (response.status === 200) {
-        console.log('Showing all news..')
-        let result = await response.json();
 
-        let html = "";
+        let allNews = await response.json();
 
-        for (let n of result) {
-            html += `<p>Id: ${n.id} Rubrik: ${n.header}</p>`;
+        console.log("allNews", allNews);
+
+        let html = "<table><tr><th>Id</th><th>Header</th><th>Intro</th><th>Created</th><th>Updated</th><th></th></tr>";
+
+        for (let n of allNews) {
+            html += `<tr><td>${n.id}</td><td>${n.header}</td><td>${n.intro}</td><td>${n.createdDate}</td><td>${n.updatedDate}</td><td><button>Update</button></td></tr>`;
         }
 
-        document.getElementById("newsList").innerHTML = html;
+        document.getElementById("newsList").innerHTML = html + `</table>`;
 
-        //TIPS högerklicka på objekt stora as global variabel--> laborera med resultatet
 
-        //let listOfNews = [];
-
-        //for (var i = 0; i < result.length; i++) {
-        //    listOfNews.push(result[i]);
-        //    //document.getElementById("newsList").innerHTML += result[i] + `<br>`;
-        //}
-
-        ////JSON.stringify();
-
-        //console.table(listOfNews);
-
-        //let newsList = document.getElementById("newsList");
-
-        //for (var i = 0; i < listOfNews.length; i++) {
-        //    let post = JSON.parse(listOfNews[i]);
-        //    newsList.innerText = post.id + " " + post.header;
-
-        //}
-
-        //Fyller tabellen med X rader , x = antalet posts
-
-                //let row = document.createElement("tr");
-                //let newsTable = document.getElementById("newsTable");
-                //newsTable.appendChild(row);
-
-                //for (var i = 0; i < 7; i++) {
-                //    let x = row.insertCell(i);
-                //    x.innerHTML = "Data";
-                //}
-
-           
-
-        
-
-        //Skapar en ny row med en cell
-
-        //for (var i = 0; i < 7; i++) {
-        //    let row = document.getElementById("myRow");
-        //    let x = row.insertCell(0);
-        //    x.innerHTML = "Data";
-        //}
-
-        //var row = document.getElementById("myRow");
-        //var x = row.insertCell(0);
-        //x.innerHTML = "New cell";
-
-        //document.getElementById("newsList");
-
-        //for (var i = 0; i < listOfNews.length; i++) {
-        //    document.getElementById("newsList").innerText += listOfNews[i].;
-
-        //}
-
-        //document.getElementById("newsList").innerText = listOfNews;
     }
     else if (response.status === 500) {
         console.log('Something went wrong..');
